ab1bf05399637031c906f6afe1a4dea8d96d9553
Rajesh Mohanty <mohantyrajesh1984@gmail.com>
2021-09-24 14:08:57 +0530

    memo usage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from "react";
-import User from "./components/User";
+import React, { useState } from "react";
+import { PureCat } from "./components/Cat";
 
 function App() {
+  const [cats, setCats] = useState(["Biscute", "Jungle", "Outlaw"]);
   return (
     <div className="App" style={{ padding: "15px" }}>
       <header className="App-header">
-        <User />
+        {cats.map((name, i) => (
+          <PureCat
+            key={i}
+            name={name}
+            meow={(name) => console.log(`${name} has meowed`)}
+          />
+        ))}
+        <button onClick={() => setCats([...cats, prompt("Name cat")])}>
+          Add a cat
+        </button>
         {/*
+        <User />
         <AddColorForm />
         <ColorList />
         */}
---
+++ src/components/Cat.js
@@ -0,0 +1,12 @@
+import React, { memo } from "react";
+
+export const Cat = ({ name, meow = (f) => f }) => {
+  console.log(`rendering ${name}`);
+
+  return <p onClick={() => meow(name)}>{name}</p>;
+};
+
+export const PureCat = memo(
+  Cat,
+  (prevProps, nextProps) => prevProps.name === nextProps.name,
+);
