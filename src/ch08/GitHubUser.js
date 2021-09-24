afc656bee774b1aed28092abbee5013615a72c83
Rajesh Mohanty <mohantyrajesh1984@gmail.com>
2021-09-24 18:31:00 +0530

    fetch api update2

--- src/ch08/GitHubUser.js
+++ src/ch08/GitHubUser.js
@@ -1,16 +1,30 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 
-async function getUserInfo(login) {
-  const res = await fetch(`https://api.github.com/users/${login}`);
-  const userData = await res.json();
-  return userData;
-}
+const loadJSON = (key) => key && JSON.parse(localStorage.getItem(key));
+const saveJSON = (key, data) => localStorage.setItem(key, JSON.stringify(data));
+
 function GitHubUser({ login }) {
-  const [data, setData] = useState();
+  const [data, setData] = useState(loadJSON(`user:${login}`));
+
+  useEffect(() => {
+    if (!data) return;
+    if (data.login === login) return;
+    const { name, avatar_url, location } = data;
+    saveJSON(`user:${login}`, {
+      name,
+      login,
+      avatar_url,
+      location,
+    });
+  }, [data]);
 
   useEffect(() => {
     if (!login) return;
-    getUserInfo(login).then(setData).catch(console.error);
+    if (data && data.login === login) return;
+    fetch(`https://api.github.com/users/${login}`)
+      .then((response) => response.json())
+      .then(setData)
+      .catch(console.error);
   }, [login]);
 
   if (data) return <pre>{JSON.stringify(data, null, 2)}</pre>;
@@ -18,15 +32,40 @@ function GitHubUser({ login }) {
   return null;
 }
 
-export default function App() {
-  const [user, setUser] = useState();
+function GitHubUserV2({ login }) {
+  const [data, setData] = useState();
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState();
+
+  useEffect(() => {
+    if (!login) return;
+    setLoading(true);
+    fetch(`https://api.github.com/users/${login}`)
+      .then((res) => res.json())
+      .then(setData)
+      .then(() => setLoading(false))
+      .catch(setError);
+  }, [login]);
+
+  if (loading) return <h1>Loading...</h1>;
+  if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
+  if (!data) return null;
 
   return (
+    <div className="githubUser">
+      <img src={data.avatar_url} alt={data.login} style={{ width: 200 }}></img>
+      <div>
+        <h1>{data.login}</h1>
+        {data.name && <p> {data.name}</p>}
+        {data.location && <p>{data.location}</p>}
+      </div>
+    </div>
+  );
+}
+export default function App() {
+  return (
     <>
-      <button onClick={() => setUser(prompt("Enter git hub user id"))}>
-        Get User{" "}
-      </button>
-      <GitHubUser login={user} />
+      <GitHubUserV2 login="moonhighway" />
     </>
   );
 }
